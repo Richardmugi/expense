cd69232dfc6ae97c92ce8d30dcf5edb2e741b773
Richardmugi <113356354+Richardmugi@users.noreply.github.com>
2023-08-04 14:45:01 +0300

    Add files via upload

--- lib/goals.dart
+++ lib/goals.dart
@@ -1,21 +1,230 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class ExpenseGoalPage extends StatefulWidget {
+class BudgetPage extends StatefulWidget {
   @override
-  _ExpenseGoalPageState createState() => _ExpenseGoalPageState();
+  _BudgetPageState createState() => _BudgetPageState();
 }
 
-class _ExpenseGoalPageState extends State<ExpenseGoalPage> {
-  double _weeklyExpenseGoal = 0.0;
-  final _formKey = GlobalKey<FormState>();
+class _BudgetPageState extends State<BudgetPage> {
+  double _weeklyGoal = 0;
+  double _monthlyGoal = 0;
+  double _weeklyTotal = 0;
+  double _monthlyTotal = 0;
+  String? _selectedBudgetType;
+  // New variable to store the selected budget type.
 
-  void _saveExpenseGoal() {
-    if (_formKey.currentState!.validate()) {
-      _formKey.currentState!.save();
-      // TODO: Save the _weeklyExpenseGoal to your desired storage (e.g., SharedPreferences, database).
-      Navigator.pop(
-          context); // Navigate back to the previous screen after saving.
+  void _saveGoals() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setDouble('weekly_goal', _weeklyGoal);
+    prefs.setDouble('monthly_goal', _monthlyGoal);
+    prefs.setString('selected_budget_type',
+        _selectedBudgetType!); // Save the selected budget type.
+  }
+
+  void _loadGoals() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _weeklyGoal = prefs.getDouble('weekly_goal') ?? 0;
+      _monthlyGoal = prefs.getDouble('monthly_goal') ?? 0;
+      _selectedBudgetType = prefs
+          .getString('selected_budget_type'); // Load the selected budget type.
+    });
+  }
+
+  void _loadRecordsTotal() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    List<String> recordStrings = prefs.getStringList('expense_records') ?? [];
+    List<Map<String, dynamic>> records = recordStrings.map((record) {
+      return Map<String, dynamic>.from(json.decode(record));
+    }).toList();
+
+    DateTime now = DateTime.now();
+    int currentWeek = now.weekday;
+    int currentMonth = now.month;
+
+    double weeklyTotal = 0;
+    double monthlyTotal = 0;
+
+    for (var record in records) {
+      double amount = record['amount'];
+      DateTime recordDate = DateTime.parse(record['date']);
+      int recordWeek = recordDate.weekday;
+      int recordMonth = recordDate.month;
+
+      // Calculate total amount for weekly budget
+      if (_selectedBudgetType == 'Weekly' && currentWeek == recordWeek) {
+        weeklyTotal += amount;
+      }
+
+      // Calculate total amount for monthly budget
+      if (_selectedBudgetType == 'Monthly' && currentMonth == recordMonth) {
+        monthlyTotal += amount;
+      }
+    }
+
+    setState(() {
+      _weeklyTotal = weeklyTotal;
+      _monthlyTotal = monthlyTotal;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _loadGoals();
+    _loadRecordsTotal();
+  }
+
+  Widget _buildGoalContainer({
+    required String title,
+    required double goalAmount,
+    required double totalAmount,
+    required VoidCallback onTap,
+  }) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: EdgeInsets.all(16),
+        margin: EdgeInsets.symmetric(vertical: 8),
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              title,
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Goal: \$${goalAmount.toStringAsFixed(2)}',
+              style: TextStyle(fontSize: 16),
+            ),
+            if (totalAmount > 0) ...[
+              SizedBox(height: 10),
+              Text(
+                'Amount used: \$${totalAmount.toStringAsFixed(2)}',
+                style: TextStyle(fontSize: 16),
+              ),
+            ],
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: onTap,
+              child: Text('Set Goal'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _onSetWeeklyGoal() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Set Weekly Goal'),
+          content: TextFormField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(labelText: 'Amount (\$)'),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter a valid amount';
+              }
+              final double amount = double.tryParse(value) ?? 0.0;
+              if (amount <= 0) {
+                return 'Amount must be greater than zero';
+              }
+              return null;
+            },
+            onChanged: (value) {
+              setState(() {
+                _weeklyGoal = double.tryParse(value) ?? 0.0;
+              });
+            },
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                _saveGoals();
+                _selectedBudgetType = 'Weekly'; // Set the selected budget type.
+                Navigator.pop(context);
+              },
+              child: Text('Save'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _onSetMonthlyGoal() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Set Monthly Goal'),
+          content: TextFormField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(labelText: 'Amount (\$)'),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter a valid amount';
+              }
+              final double amount = double.tryParse(value) ?? 0.0;
+              if (amount <= 0) {
+                return 'Amount must be greater than zero';
+              }
+              return null;
+            },
+            onChanged: (value) {
+              setState(() {
+                _monthlyGoal = double.tryParse(value) ?? 0.0;
+              });
+            },
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                _saveGoals();
+                _selectedBudgetType =
+                    'Monthly'; // Set the selected budget type.
+                Navigator.pop(context);
+              },
+              child: Text('Save'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  String _getBudgetStatus(double goal, double totalExpenses) {
+    double remainingBudget = goal - totalExpenses;
+    double percentageUsed = (totalExpenses / goal) * 100;
+
+    if (totalExpenses > goal) {
+      return 'You have exceeded your budget by \$${totalExpenses - goal}!';
+    } else if (percentageUsed >= 80) {
+      return 'You are almost completing your budget (Remaining: \$${remainingBudget.toStringAsFixed(2)})';
+    } else {
+      return 'You are within your budget (Remaining: \$${remainingBudget.toStringAsFixed(2)})';
     }
   }
 
@@ -23,43 +232,38 @@ class _ExpenseGoalPageState extends State<ExpenseGoalPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Set Weekly Expense Goal'),
+        title: Text('Set Budget Goals'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              TextFormField(
-                keyboardType: TextInputType.number,
-                inputFormatters: [
-                  FilteringTextInputFormatter.allow(RegExp(r'^\d+\.?\d{0,2}'))
-                ],
-                decoration:
-                    InputDecoration(labelText: 'Weekly Expense Goal (\$)'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter a valid amount';
-                  }
-                  final double amount = double.tryParse(value) ?? 0.0;
-                  if (amount <= 0) {
-                    return 'Amount must be greater than zero';
-                  }
-                  return null;
-                },
-                onSaved: (value) {
-                  _weeklyExpenseGoal = double.tryParse(value!) ?? 0.0;
-                },
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            _buildGoalContainer(
+              title: 'Set Weekly Goal:',
+              goalAmount: _weeklyGoal,
+              totalAmount: _weeklyTotal,
+              onTap: _onSetWeeklyGoal,
+            ),
+            SizedBox(height: 20),
+            _buildGoalContainer(
+              title: 'Set Monthly Goal:',
+              goalAmount: _monthlyGoal,
+              totalAmount: _monthlyTotal,
+              onTap: _onSetMonthlyGoal,
+            ),
+            SizedBox(height: 20),
+            if (_selectedBudgetType != null && _selectedBudgetType == 'Weekly')
+              Text(
+                _getBudgetStatus(_weeklyGoal, _weeklyTotal),
+                style: TextStyle(fontSize: 16, color: Colors.red),
               ),
-              SizedBox(height: 16.0),
-              ElevatedButton(
-                onPressed: _saveExpenseGoal,
-                child: Text('Save'),
+            if (_selectedBudgetType != null && _selectedBudgetType == 'Monthly')
+              Text(
+                _getBudgetStatus(_monthlyGoal, _monthlyTotal),
+                style: TextStyle(fontSize: 16, color: Colors.red),
               ),
-            ],
-          ),
+          ],
         ),
       ),
     );
--- lib/records.dart
+++ lib/records.dart
@@ -27,43 +27,77 @@ class _RecordsPageState extends State<RecordsPage> {
     });
   }
 
+  double _calculateTotalExpenses() {
+    return _expenseRecords.length.toDouble();
+  }
+
+  double _calculateTotalAmount() {
+    double totalAmount = 0;
+    for (var record in _expenseRecords) {
+      totalAmount += record['amount'];
+    }
+    return totalAmount;
+  }
+
   @override
   Widget build(BuildContext context) {
+    double totalExpenses = _calculateTotalExpenses();
+    double totalAmount = _calculateTotalAmount();
     return Scaffold(
       appBar: AppBar(
-        title: Text('Expense Records'),
+        title: Text('Expense Records '),
+        backgroundColor: Colors.purple,
       ),
       body: _expenseRecords.isNotEmpty
           ? ListView(
-              children: List.generate(_expenseRecords.length, (index) {
-                final expenseRecord = _expenseRecords[index];
-                return Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 8.0),
-                  child: ListTile(
-                    title: Text('Expense: ${expenseRecord['expense']}'),
-                    subtitle: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text('Quantity: ${expenseRecord['quantity']}'),
-                        Text('Amount: \$${expenseRecord['amount']}'),
-                        Text('Date: ${expenseRecord['date']}'),
-                      ],
+              children: [
+                ...List.generate(_expenseRecords.length, (index) {
+                  final expenseRecord = _expenseRecords[index];
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                    child: ListTile(
+                      title: Text('Expense: ${expenseRecord['expense']}'),
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Quantity: ${expenseRecord['quantity']}'),
+                          Text('Amount: \$${expenseRecord['amount']}'),
+                          Text('Date: ${expenseRecord['date']}'),
+                        ],
+                      ),
+                      trailing: IconButton(
+                        icon: Icon(Icons.delete),
+                        onPressed: () {
+                          _showDeleteConfirmationDialog(index);
+                        },
+                      ),
                     ),
-                    trailing: IconButton(
-                      icon: Icon(Icons.delete),
-                      onPressed: () {
-                        _showDeleteConfirmationDialog(index);
-                      },
-                    ),
+                  );
+                }),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    children: [
+                      Text(
+                        'Total Expenses: $totalExpenses',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        'Total Amount: \$${totalAmount.toStringAsFixed(2)}',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                    ],
                   ),
-                );
-              }),
+                ),
+              ],
             )
           : Center(
               child: Text('No expense records found.'),
             ),
       bottomNavigationBar: BottomNavigationBar(
-        currentIndex: 1, // Set the current index for the RecordsPage to 1
+        currentIndex: 1,
         items: [
           BottomNavigationBarItem(
             icon: Icon(Icons.category),
@@ -76,13 +110,8 @@ class _RecordsPageState extends State<RecordsPage> {
         ],
         onTap: (index) {
           if (index == 0) {
-            // Navigate to the ExpenseTrackingPage (Categories page)
-            Navigator.pushNamed(
-              context,
-              '/third',
-            );
+            Navigator.pushNamed(context, '/third');
           }
-          // If the index is 1, stay on the RecordsPage (do nothing).
         },
       ),
     );
